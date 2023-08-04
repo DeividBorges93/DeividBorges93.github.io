2da606928184d1fc780c2aee81797cc36afa74b7
DeividBorges93 <deividmborges@gmail.com>
2023-08-04 16:36:30 -0300

    fix(style-header): :iphone: change header a fixed header
    for accessibility reasons I decided to make the header fixed.
    
    added some tweaks to the pages to adapt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,53 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../css/components/header.css';
 import { NavLink } from 'react-router-dom';
 
 export default function Header() {
-  const [isHeaderVisible, setHeaderVisible] = useState(true);
-
-  const handleMouseEnter = () => {
-    setHeaderVisible(true);
-  };
-
-  const handleMouseLeave = () => {
-    setHeaderVisible(false);
-  };
-
   return (
-    <div
-      className={`header ${isHeaderVisible ? '' : 'hidden'}`}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
+    <div className='header'>
+      
       <div className="container-header">
-      <NavLink
-          className="link-menu"
-          activeclassname="active"
-          to='/inicio'
-        >
-          Início
-        </NavLink>
         <NavLink
-          className="link-menu"
-          activeclassname="active"
-          to='/sobremim'
-        >
-          Sobre mim
-        </NavLink>
-        <NavLink
-          className="link-menu"
-          activeclassname="active"
-          to='/contato'
-        >
-          Contato
-        </NavLink>
-        <NavLink
-          className="link-menu"
-          activeclassname="active"
-          to='/projetos'
-        >
-          Projetos
-        </NavLink>
+            className="link-menu"
+            activeclassname="active"
+            to='/inicio'
+          >
+            Início
+          </NavLink>
+          <NavLink
+            className="link-menu"
+            activeclassname="active"
+            to='/sobremim'
+          >
+            Sobre mim
+          </NavLink>
+          <NavLink
+            className="link-menu"
+            activeclassname="active"
+            to='/contato'
+          >
+            Contato
+          </NavLink>
+          <NavLink
+            className="link-menu"
+            activeclassname="active"
+            to='/projetos'
+          >
+            Projetos
+          </NavLink>
       </div>
     </div>
   );
