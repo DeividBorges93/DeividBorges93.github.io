7841dc7f2e233ca3d58f29eaeb08f786f8f0dcce
DeividBorges93 <deividmborges@gmail.com>
2023-08-10 15:18:13 -0300

    feat(footer): :sparkles: render footer component in the pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,14 @@
 import Header from "../components/Header"
 import SendEmail from "../components/SendEmail"
+import Footer from "../components/Footer";
 
 export default function Contact() {
   return (
     <div>
       <Header />
       <SendEmail />
+      <Footer />
+
     </div>
   )
 };
